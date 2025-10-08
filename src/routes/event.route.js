6cf4d2c5b925ae7442578e1419a14e5de2d431f2
Expacio <akshay.mishra2228@gmail.com>
2025-10-08 17:59:34 +0530

    readability

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controller/event.controller');
 
+router.get('/', eventController.fetchAll);
+router.get('/:id', eventController.fetchEvent);
 router.post('/createEvent', eventController.createEvent);
 router.put('/editEvent', eventController.editEvent);
 router.delete('/deleteEvent/:id', eventController.deleteEvent);
-router.get('/fetchEvent/:id', eventController.fetchEvent);
-router.get('/fetchAll', eventController.fetchAll)
 module.exports = router;
