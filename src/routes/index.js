45f9d144ab1f963cb25260e89d7bec2ebc296750
Diptayan Jash <dj2037@srmist.edu.in>
2025-10-22 01:47:04 +0530

    feat: Add recruitment routes to the main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,13 @@ const sponsorRoutes = require('./sponsor.route');
 const eventRoutes = require('./event.route');
 const contactRoutes = require('./contact.route');
 const certificateRoutes = require('./certificate.route');
+const recruitmentRoutes = require('./recruitment.route');
 
 router.use('/team', teamRoutes);
 router.use('/sponsors', sponsorRoutes);
 router.use('/events', eventRoutes);
 router.use('/contact', contactRoutes);
 router.use('/certificate', certificateRoutes);
+router.use('/recruitment', recruitmentRoutes);
 
 module.exports = router;
